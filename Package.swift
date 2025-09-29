bcd755d6a753cdd0e2e88fd783dd42f31389a658
djdreamfix <djdreamfix@gmail.com>
2025-09-29 22:17:59 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,8 @@ import PackageDescription
 let package = Package(
     name: "LaserDetector",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v15),
+        .macOS(.v11)
     ],
     products: [
         // Продукт, який можна запускати

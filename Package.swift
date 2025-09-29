2bbddbf8ef88e159bbb951c184297d7c46f55c4f
djdreamfix <djdreamfix@gmail.com>
2025-09-29 21:55:47 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -7,20 +7,23 @@ let package = Package(
         .iOS(.v15)
     ],
     products: [
-        .executableTarget(
+        // Продукт, який можна запускати
+        .executable(
             name: "LaserDetector",
             targets: ["LaserDetector"]
-        ),
+        )
     ],
     targets: [
+        // Основний таргет програми
         .executableTarget(
             name: "LaserDetector",
             path: "Sources/LaserDetector"
         ),
+        // Тестовий таргет, який використовує тільки LaserDetectorTests.swift
         .testTarget(
             name: "LaserDetectorTests",
             dependencies: ["LaserDetector"],
             path: "Tests/LaserDetectorTests"
-        ),
+        )
     ]
 )
